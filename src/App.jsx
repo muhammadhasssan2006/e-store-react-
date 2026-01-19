5382f702159452ec9171010b4d8563cfea0e59f8
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-01-19 22:00:30 +0500

    store api called

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Appbar from './components/Appbar'
 import './App.css'
 import { colors } from '@mui/material'
+import axios from 'axios'
+import Cards from './components/card'
+
+
 
 function App() {
+  const [products,setproducts]=useState([])
+  useEffect(()=>{
+    axios.get("https://fakestoreapi.com/products")
+    .then((res)=> setproducts(res.data))
+
+  },[])
+  console.log(products)
 
   return (
     <>
     <Appbar   bgcolor="#121212" />
-      <h1>hello</h1>
+    {/* <div>
+      {
+        products && products.map((e,i)=>{
+          return(
+            <h1>{e.title}</h1>
+          )
+        })
+      }
+    </div> */}
+    <Cards />
     </>
   )
 }
