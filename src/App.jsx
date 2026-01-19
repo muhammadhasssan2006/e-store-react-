15b7e2cd3c0fa4e1af5a3aa6a077231c71efb102
muhammadhasssan2006 <hassanweb365@gmail.com>
2026-01-19 21:20:01 +0500

    store navbar done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Appbar from './components/Appbar'
+import './App.css'
+import { colors } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+    <Appbar   bgcolor="#121212" />
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
